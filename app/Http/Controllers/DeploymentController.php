fc01ff89b66a4003e9285c91920c15309df9ccb4
qiezi0301 <qiezi0301@163.com>
2019-01-21 22:14:48 +0800

    xiugai

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -7,17 +7,22 @@ use Illuminate\Http\Request;
 class DeploymentController extends Controller {
     public function deploy(Request $request) {
         $commands = ['cd /var/www/gitlaravel', 'git pull'];
-        $signature = $request->header('X-Hub-Signature');
-        // $headers = getallheaders(); $headers['X-Hub-Signature']
-        $payload = file_get_contents('php://input');
-        if ($this->isFromGithub($payload, $signature)) {
-            foreach ($commands as $command) {
-                shell_exec($command);
-            }
-            http_response_code(200);
-        } else {
-            abort(403);
+
+        foreach ($commands as $command) {
+            shell_exec($command);
         }
+        http_response_code(200);
+//        $signature = $request->header('X-Hub-Signature');
+//        // $headers = getallheaders(); $headers['X-Hub-Signature']
+//        $payload = file_get_contents('php://input');
+//        if ($this->isFromGithub($payload, $signature)) {
+//            foreach ($commands as $command) {
+//                shell_exec($command);
+//            }
+//            http_response_code(200);
+//        } else {
+//            abort(403);
+//        }
     }
 
     private function isFromGithub($payload, $signature) {
